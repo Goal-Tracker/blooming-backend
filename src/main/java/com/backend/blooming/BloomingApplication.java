bc62645bcaae1a56f654f200b4bdc440928a98e7
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-10-28 18:29:50 +0900

    chore: 프로젝트 기초 세팅

---
+++ src/main/java/com/backend/blooming/BloomingApplication.java
@@ -0,0 +1,13 @@
+package com.backend.blooming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloomingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloomingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/blooming/BloomingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.blooming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloomingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
