1863d9954f67656f669cf5d0b896cd477f3aadb9
jhsseonn <hs125bt@gmail.com>
2023-11-12 14:59:38 +0900

    feat: 골 팀 도메인 클래스 생성 및 연관관계 매핑

--- src/main/java/com/backend/blooming/goal/domain/Goal.java
+++ src/main/java/com/backend/blooming/goal/domain/Goal.java
@@ -1,17 +1,37 @@
 package com.backend.blooming.goal.domain;
 
-import jakarta.persistence.*;
-import lombok.*;
+
+import com.backend.blooming.goal.application.util.DateFormat;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.Builder;
+import lombok.AccessLevel;
 import org.hibernate.annotations.CreationTimestamp;
 
+import java.text.ParseException;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @EqualsAndHashCode(of = "id", callSuper = false)
 @ToString
-public class Goal {
+public class Goal extends DateFormat {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -32,8 +52,8 @@ public class Goal {
     @Column(nullable = false)
     private int goalDays;
 
-    @Column(nullable = false)
-    private boolean goalAction;
+    @OneToMany(mappedBy = "goal", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<GoalTeam> goalTeams = new ArrayList<>();
 
     @CreationTimestamp
     @Column(nullable = false, length = 20, updatable = false)
@@ -44,20 +64,36 @@ public class Goal {
     private LocalDateTime updatedAt;
 
     @Builder
-    public Goal(
+    private Goal(
             final String goalName,
             final String goalMemo,
             final String goalStartDay,
             final String goalEndDay,
-            final int goalDays,
-            final boolean goalAction
+            final int goalDays
     ) {
         this.goalName = goalName;
         this.goalMemo = goalMemo;
         this.goalStartDay = goalStartDay;
         this.goalEndDay = goalEndDay;
         this.goalDays = goalDays;
-        this.goalAction = goalAction;
     }
+
+    public boolean isGoalAvailable() throws ParseException {
+        final Date goalStartDate = dateFormatter(goalStartDay);
+        final Date goalEndDate = dateFormatter(goalEndDay);
+        final Date nowDate = dateFormatter(LocalDate.now().toString());
+
+        if (nowDate.compareTo(goalStartDate)<=0 && nowDate.compareTo(goalEndDate)>=0) {
+            return true;
+        }
+
+        return false;
+    }
+
+    // 골 제목과 메모에 글자 수 제한을 둘 것인지 논의 필요
+    // 골 시작일이 오늘 날짜보다 이전일 경우 생성불가
+    // 골 마감일이 오늘 날짜보다 이후일 경우 생성불가
+    // 골 시작일보다 골 마감일이 더 빠를 경우 생성불가
+    // 골 일수가 1보다 작을 경우 생성불가
 
 }
---
+++ src/main/java/com/backend/blooming/goal/domain/GoalTeam.java
@@ -0,0 +1,37 @@
+package com.backend.blooming.goal.domain;
+
+import com.backend.blooming.user.domain.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@EqualsAndHashCode(of = "id", callSuper = false)
+@ToString
+public class GoalTeam {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "goal_team_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "id")
+    private Goal goal;
+}
