6d66c801a825d83d63ae3423c5dcfb22e3b4d3b1
jhsseonn <hs125bt@gmail.com>
2023-11-12 19:30:04 +0900

    feat: 사용하지 않는 패키지 정리

--- src/main/java/com/backend/blooming/goal/domain/Goal.java
+++ src/main/java/com/backend/blooming/goal/domain/Goal.java
@@ -9,7 +9,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Column;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
