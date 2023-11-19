d86a08c02b35e65173e02f1b128e29f7cd2304b8
jhsseonn <hs125bt@gmail.com>
2023-11-19 13:37:37 +0900

    feat: 골 추가

---
+++ src/main/java/com/backend/blooming/goal/presentation/GoalController.java
@@ -0,0 +1,33 @@
+package com.backend.blooming.goal.presentation;
+
+import com.backend.blooming.goal.application.GoalService;
+import com.backend.blooming.goal.application.dto.CreateGoalDto;
+import com.backend.blooming.goal.presentation.dto.request.CreateGoalRequest;
+import com.backend.blooming.goal.presentation.dto.response.CreateGoalResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+import java.text.ParseException;
+
+@RestController
+@RequestMapping("/goals")
+@RequiredArgsConstructor
+public class GoalController {
+
+    private final GoalService goalService;
+
+    @PostMapping(value = "/add", headers = "X-API-VERSION=1")
+    public ResponseEntity<CreateGoalResponse> createGoal(@RequestPart(required = false) @Valid CreateGoalRequest request) throws ParseException {
+        final CreateGoalDto createGoalDto = goalService.createGoalDto(request);
+        final CreateGoalResponse response = goalService.createGoalResponse(createGoalDto);
+
+        return ResponseEntity.created(URI.create("/goals/"+response.goalId())).body(response);
+    }
+
+}
