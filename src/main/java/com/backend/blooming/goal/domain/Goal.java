5dc5096e6fa531efeac949b04346a74d3b1f1d5a
jhsseonn <hs125bt@gmail.com>
2023-11-27 03:26:19 +0900

    refactor: 골 유효성 검증 메서드 리팩토링

--- src/main/java/com/backend/blooming/goal/domain/Goal.java
+++ src/main/java/com/backend/blooming/goal/domain/Goal.java
@@ -2,24 +2,23 @@ package com.backend.blooming.goal.domain;
 
 import com.backend.blooming.goal.application.util.DateFormat;
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.Column;
+import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import lombok.Builder;
-import lombok.AccessLevel;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.annotations.CreationTimestamp;
 
-import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -69,11 +68,6 @@ public class Goal extends DateFormat {
     @Column(length = 20)
     private LocalDateTime updatedAt;
 
-    public void addGoalTeam(GoalTeam goalTeam){
-        goalTeams.add(goalTeam);
-        goalTeam.updateGoal(this);
-    }
-
     @Builder
     private Goal(
             final String goalName,
@@ -84,12 +78,8 @@ public class Goal extends DateFormat {
     ){
         this.goalName = goalName;
         this.goalMemo = goalMemo;
-        validStartDay(goalStartDay);
-        validEndDay(goalEndDay);
-        validGoal(goalStartDay, goalEndDay);
         this.goalStartDay = goalStartDay;
         this.goalEndDay = goalEndDay;
-        validGoalDays(goalDays);
         this.goalDays = goalDays;
         this.goalTeams = new ArrayList<>();
     }
@@ -107,32 +97,6 @@ public class Goal extends DateFormat {
 
     public void updateGoalTeams(List<GoalTeam> goalTeams){
         this.goalTeams = goalTeams;
-    }
-
-    public void validStartDay(Date goalStartDay) {
-        final Date nowDate = dateFormatter(LocalDate.now().toString());
-        if (goalStartDay.compareTo(nowDate)<0){
-            throw new IllegalArgumentException("시작 날짜는 현재 날짜 이후여야합니다.");
-        }
-    }
-
-    public void validEndDay(Date goalEndDay) {
-        final Date nowDate = dateFormatter(LocalDate.now().toString());
-        if (nowDate.compareTo(goalEndDay)>0){
-            throw new IllegalArgumentException("종료 날짜는 현재 날짜 이후여야합니다.");
-        }
-    }
-
-    public void validGoal(Date goalStartDay, Date goalEndDay){
-        if (goalStartDay.compareTo(goalEndDay)>0){
-            throw new IllegalArgumentException("시작 날짜는 종료 날짜보다 이전이어야합니다.");
-        }
-    }
-
-    public void validGoalDays(int goalDays){
-        if (goalDays<1){
-            throw new IllegalArgumentException("골 일자는 1 이상이어야합니다.");
-        }
     }
 
     public List<Long> getGoalTeamIds(){
