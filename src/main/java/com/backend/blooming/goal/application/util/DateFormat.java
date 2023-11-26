11f87b77a2d5ffbeff5c962a9faea84cccc1ab65
jhsseonn <hs125bt@gmail.com>
2023-11-27 00:12:11 +0900

    refactor: 컨벤션 수정

--- src/main/java/com/backend/blooming/goal/application/util/DateFormat.java
+++ src/main/java/com/backend/blooming/goal/application/util/DateFormat.java
@@ -5,20 +5,20 @@ import lombok.extern.slf4j.Slf4j;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
 @Slf4j
 public class DateFormat {
 
     public Date dateFormatter(String date){
         SimpleDateFormat formatter;
         Date dateFormatted = new Date();
-        
+
         try{
             formatter = new SimpleDateFormat("yyyy-MM-dd");
-
             dateFormatted = formatter.parse(date);
 
             return dateFormatted;
-        } catch (ParseException e){
+        } catch (ParseException e) {
             log.info("날짜 파싱 실패");
         }
 
