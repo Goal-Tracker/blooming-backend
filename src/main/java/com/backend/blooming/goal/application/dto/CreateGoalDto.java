9a8b20b7e903b8bd0b1b030467f69b6415a5a0e1
jhsseonn <hs125bt@gmail.com>
2023-11-27 00:04:27 +0900

    feat: 생성자 from 메서드 추가

--- src/main/java/com/backend/blooming/goal/application/dto/CreateGoalDto.java
+++ src/main/java/com/backend/blooming/goal/application/dto/CreateGoalDto.java
@@ -1,8 +1,11 @@
 package com.backend.blooming.goal.application.dto;
 
+import com.backend.blooming.goal.presentation.dto.request.GoalRequest;
+
 import java.util.List;
 
 public record CreateGoalDto(
+        Long goalId,
         String goalName,
         String goalMemo,
         String goalStartDay,
@@ -10,4 +13,15 @@ public record CreateGoalDto(
         int goalDays,
         List<Long> goalTeamUserIds
 ) {
+    public static CreateGoalDto from(final GoalRequest request){
+        return new CreateGoalDto(
+                request.goalId(),
+                request.goalName(),
+                request.goalMemo(),
+                request.goalStartDay(),
+                request.goalEndDay(),
+                request.goalDays(),
+                request.goalTeamUserIds()
+        );
+    }
 }
