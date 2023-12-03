1f15a7a9a9e021d73cea02294359df47368e242f
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-12-03 19:36:07 +0900

    feat: 친구 레포지토리 추가

---
+++ src/main/java/com/backend/blooming/friend/infrastructure/repository/FriendRepository.java
@@ -0,0 +1,7 @@
+package com.backend.blooming.friend.infrastructure.repository;
+
+import com.backend.blooming.friend.domain.Friend;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FriendRepository extends JpaRepository<Friend, Long> {
+}
