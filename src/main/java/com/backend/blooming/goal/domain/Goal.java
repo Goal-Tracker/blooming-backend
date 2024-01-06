b0f7705b9809ee1f8a48afda735efe2148e35de9
jhsseonn <hs125bt@gmail.com>
2024-01-06 20:35:16 +0900

    refactor: 골 제목 길이 제한

--- src/main/java/com/backend/blooming/goal/domain/Goal.java
+++ src/main/java/com/backend/blooming/goal/domain/Goal.java
@@ -31,7 +31,7 @@ public class Goal extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 50)
     private String name;
 
     @Column(columnDefinition = "text")
