d00f387d2e49d9c268be7b00ab6a2271b01f35f8
JJ503 <63184334+JJ503@users.noreply.github.com>
2024-02-07 19:36:15 +0900

    refactor: valid 관련 예외 메시지 추가

--- src/main/java/com/backend/blooming/admin/controller/dto/RequestFriendRequest.java
+++ src/main/java/com/backend/blooming/admin/controller/dto/RequestFriendRequest.java
@@ -4,12 +4,12 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Positive;
 
 public record RequestFriendRequest(
-        @NotEmpty
-        @Positive
+        @NotEmpty(message = "친구 요청자가 입력되지 않았습니다.")
+        @Positive(message = "친구 요청자의 아이디가 잘못됐습니다.")
         Long requestUser,
 
-        @NotEmpty
-        @Positive
+        @NotEmpty(message = "친구 요청 수신자가 입력되지 않았습니다.")
+        @Positive(message = "친구 요청 수신자의 아이디가 잘못됐습니다.")
         Long requestedUser
 ) {
 }
