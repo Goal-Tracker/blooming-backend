43752e8a96f58115282f40ab1fa3a1b14161c756
JJ503 <63184334+JJ503@users.noreply.github.com>
2024-02-23 07:32:32 +0900

    refactor: 새로 정해진 기능 명세에 따라 사용자 신고 로직 수정
    - 다른 신고에서는 신고 대상자(사용자)는 없어짐
    - 신고를 카테고리가 아닌 api로 구분함

--- src/main/java/com/backend/blooming/report/application/UserReportService.java
+++ src/main/java/com/backend/blooming/report/application/UserReportService.java
@@ -2,7 +2,6 @@ package com.backend.blooming.report.application;
 
 import com.backend.blooming.report.application.dto.CreateUserReportDto;
 import com.backend.blooming.report.domain.Content;
-import com.backend.blooming.report.domain.ReportInformation;
 import com.backend.blooming.report.domain.UserReport;
 import com.backend.blooming.report.infrastructure.repository.UserReportRepository;
 import com.backend.blooming.user.application.exception.NotFoundUserException;
@@ -23,11 +22,8 @@ public class UserReportService {
     public Long create(final CreateUserReportDto userReportDto) {
         final User reporter = getUser(userReportDto.reporterId());
         final User reportee = getUser(userReportDto.reporteeId());
-
-        // TODO: 2024-02-20 content와 ReportInformatin을 각각 생성하지 않고 그냥 신고 도메인 내에서 생성하는 것이 더 나을까요?
         final Content content = new Content(userReportDto.content());
-        final ReportInformation reportInformation = new ReportInformation(reporter, reportee, content);
-        final UserReport userReport = new UserReport(reportInformation);
+        final UserReport userReport = new UserReport(reporter, reportee, content);
 
         return userReportRepository.save(userReport)
                                    .getId();
--- src/main/java/com/backend/blooming/report/domain/UserReport.java
+++ src/main/java/com/backend/blooming/report/domain/UserReport.java
@@ -1,11 +1,16 @@
 package com.backend.blooming.report.domain;
 
 import com.backend.blooming.common.entity.BaseTimeEntity;
+import com.backend.blooming.user.domain.User;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
@@ -17,17 +22,27 @@ import lombok.ToString;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @EqualsAndHashCode(of = "id", callSuper = false)
-@ToString
+@ToString(exclude = {"reporter", "reportee"})
 @Table
 public class UserReport extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Embedded
-    private ReportInformation reportInformation;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reporter_id", nullable = false, foreignKey = @ForeignKey(name = "fk_user_report_reporter"))
+    private User reporter;
 
-    public UserReport(final ReportInformation reportInformation) {
-        this.reportInformation = reportInformation;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reportee_id", nullable = false, foreignKey = @ForeignKey(name = "fk_user_report_reportee"))
+    private User reportee;
+
+    @Embedded
+    private Content content;
+
+    public UserReport(final User reporter, final User reportee, final Content content) {
+        this.reporter = reporter;
+        this.reportee = reportee;
+        this.content = content;
     }
 }
