2d5fa8f2914220eb2a41584ebf895d7a2b980200
JJ503 <63184334+JJ503@users.noreply.github.com>
2024-03-01 23:15:25 +0900

    feat: local 서버를 위한 s3 설정 추가

--- src/main/java/com/backend/blooming/image/configuration/S3Configuration.java
+++ src/main/java/com/backend/blooming/image/configuration/S3Configuration.java
@@ -3,11 +3,13 @@ package com.backend.blooming.image.configuration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
+@Profile("dev | prod")
 @Configuration
 public class S3Configuration {
 
