f53a016587f4ff7722bd1405f1f21b91585cc260
JJ503 <63184334+JJ503@users.noreply.github.com>
2024-03-02 13:36:43 +0900

    refactor: 불필요한 예외 처리 제거

--- src/main/java/com/backend/blooming/image/application/ImageManager.java
+++ src/main/java/com/backend/blooming/image/application/ImageManager.java
@@ -2,9 +2,7 @@ package com.backend.blooming.image.application;
 
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-
 public interface ImageManager {
 
-    String upload(final MultipartFile multipartFile) throws IOException;
+    String upload(final MultipartFile multipartFile);
 }
