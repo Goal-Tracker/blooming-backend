241c0c20ee525f1b9ee442c4d12d9d08d10b17a7
jhsseonn <hs125bt@gmail.com>
2024-03-20 21:35:03 +0900

    refactor: exception 클래스명 변경

--- src/main/java/com/backend/blooming/stamp/application/exception/ForbiddenToCreateStamp.java
+++ src/main/java/com/backend/blooming/stamp/application/exception/ForbiddenToCreateStamp.java
@@ -3,9 +3,9 @@ package com.backend.blooming.stamp.application.exception;
 import com.backend.blooming.exception.BloomingException;
 import com.backend.blooming.exception.ExceptionMessage;
 
-public class ReadStampForbiddenException extends BloomingException {
-
-    public ReadStampForbiddenException() {
-        super(ExceptionMessage.READ_STAMP_FORBIDDEN);
+public class ForbiddenToCreateStamp extends BloomingException {
+    
+    public ForbiddenToCreateStamp() {
+        super(ExceptionMessage.CREATE_STAMP_FORBIDDEN);
     }
 }
--- src/main/java/com/backend/blooming/stamp/application/exception/ForbiddenToReadStamp.java
+++ src/main/java/com/backend/blooming/stamp/application/exception/ForbiddenToReadStamp.java
@@ -3,9 +3,9 @@ package com.backend.blooming.stamp.application.exception;
 import com.backend.blooming.exception.BloomingException;
 import com.backend.blooming.exception.ExceptionMessage;
 
-public class CreateStampForbiddenException extends BloomingException {
-    
-    public CreateStampForbiddenException() {
-        super(ExceptionMessage.CREATE_STAMP_FORBIDDEN);
+public class ForbiddenToReadStamp extends BloomingException {
+
+    public ForbiddenToReadStamp() {
+        super(ExceptionMessage.READ_STAMP_FORBIDDEN);
     }
 }
